587fcd615aeec34562b5be9b7e7ba9240f5572d3
md-mojahed <work.mojahedul@gmail.com>
2021-12-24 21:56:46 +0600

    Upload file using File Class

---
+++ session7/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>FIle Upload</title>
+    </head>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <button type="submit">Upload</button>
+        </form>
+    </body>
+</html>
